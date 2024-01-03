d16b267672c81d319bdde32b7979bb24b26a01c5
calvincarja <cloth.sidle0i@icloud.com>
2024-01-03 09:52:30 -0500

    Update login function

--- login-first-project-v5.py
+++ login-first-project-v5.py
@@ -14,10 +14,10 @@ def register():
         print('your user name has been saved\nNow its time to create a password')
         print('Below are the requirements for your password: at least two upper case letters\nthree lower case letters\nat least one digit')
         password_input = input('Please create your new password: ')
-        if web_password(password_input) != 'password was accepted and saved':
+        if web_password(password_input) != 'password was accepted and saved': # varible being passed through web_password function is local, it does not need to match the varible inside the web_password function outside this function
             print('password not accepted, try again')
             continue
-        users[username] = password_input # user info is not stored
+        users[username] = password_input # user info is now stored
         break # exit loop
 
 def web_password(password):
@@ -37,7 +37,16 @@ def web_password(password):
         if upper_count >= 2 and lower_count >= 3 and digit_count >= 1:  # if meets requirement, pass the check. 
             return "password was accepted and saved" 
 
-
-print(users)
-
-
+def login ():
+    while True:
+        login_username = input('Please enter your username: ')
+        # how do i look up this user name? from the dictionary? 
+        if login_username not in users: # in keyword used with dictionarys only refernce keys, not values
+            # how do i check for values?
+            print('username is not in our system. Please try again: ')
+            continue
+        print('Username found')
+        login_password = input('Enter your password: ')
+        if login_password != users[login_username]: # the password is the value of key:value relationship, thus, password is the value for usernsame key
+            print('password is incorrect - try agian')
+            continue
